d79d4a13d50dec4ff13e48692a96981f57d5d44c
Anandi Karunaratne <anandi.karunaratne@gmail.com>
2024-08-25 13:36:37 +1000

    workshop 4

--- src/main/java/org/anandi/SWEN20003/workshops/workshop4/SimpleGame.java
+++ src/main/java/org/anandi/SWEN20003/workshops/workshop4/SimpleGame.java
@@ -9,24 +9,16 @@ public class SimpleGame extends AbstractGame {
     private final Image ballImage = new Image("res/ball.png");
     private final Font font = new Font("res/conformable.otf", 24);
 
-<<<<<<< Updated upstream
     private static final Point BALL_POSITION = new Point(650, 180);
     private static final double STEP_SIZE = 1;
     private static final double SCORE_DISTANCE = 20;
     private static final Point TEXT_POSITION = new Point(32, 32);
-=======
-    private static final double STEP_SIZE = 1;
-    private static final Point BALL_POINT = new Point(650, 180);
-    private static final Point TEXT_POINT = new Point(32, 32);
-    private static final double SCORE_DISTANCE = 20;
->>>>>>> Stashed changes
 
     private double playerX = 200;
     private double playerY = 350;
 
     @Override
     protected void update(Input input) {
-<<<<<<< Updated upstream
         if (input.wasPressed(Keys.ESCAPE)) {
             Window.close();
         } else {
@@ -50,26 +42,6 @@ public class SimpleGame extends AbstractGame {
                 font.drawString("Great job!", TEXT_POSITION.x, TEXT_POSITION.y);
             }
         }
-=======
-        if (input.isDown(Keys.LEFT)) {
-            playerX -= STEP_SIZE;
-        }
-        if (input.isDown(Keys.RIGHT)) {
-            playerX += STEP_SIZE;
-        }
-        if (input.isDown(Keys.UP)) {
-            playerY -= STEP_SIZE;
-        }
-        if (input.isDown(Keys.DOWN)) {
-            playerY += STEP_SIZE;
-        }
-
-        if (new Point(playerX, playerY).distanceTo(BALL_POINT) <= SCORE_DISTANCE) {
-            font.drawString("Great job!", TEXT_POINT.x, TEXT_POINT.y);
-        }
-        ballImage.draw(BALL_POINT.x, BALL_POINT.y);
-        playerImage.draw(playerX, playerY);
->>>>>>> Stashed changes
     }
 
     public static void main(String[] args) {

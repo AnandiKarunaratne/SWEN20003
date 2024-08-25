1164fe1f223368d429ccb252d1e0f7f1c090d08b
Anandi Karunaratne <anandi.karunaratne@gmail.com>
2024-08-25 13:26:48 +1000

    workshop 4

--- src/main/java/org/anandi/SWEN20003/Main.java
+++ src/main/java/org/anandi/SWEN20003/Main.java
@@ -8,36 +8,14 @@ import java.util.Random;
 
 public class Main {
 
-    private final static Random rand = new Random();
+//    private final static Random rand = new Random();
     public static void main(String[] args) {
-        Person aliceYan = new Person("Alice", randomPoint());
-        Person bobYan = new Person("Bob", randomPoint());
-        Person carterYan = new Person("Carter", randomPoint());
-        Person dariusYan = new Person("Darius", randomPoint());
-        Person eyreYan = new Person("Eyre", randomPoint());
-        Person[] yans = new Person[] {
-                aliceYan, bobYan, carterYan, dariusYan, eyreYan
-        };
-        Household yanHousehold = new Household("Yan", yans);
-        for (Person person: yans) {
-            person.setHousehold(yanHousehold);
-        }
-        Person aliceMcMurtry = new Person("Alice", randomPoint());
-        Person bobMcMurtry = new Person("Bob", randomPoint());
-        Person carterMcMurtry = new Person("Carter", randomPoint());
-        Person dariusMcMurtry = new Person("Darius", randomPoint());
-        Person eyreMcMurtry = new Person("Eyre", randomPoint());
-        Person[] mcMurtrys = new Person[] {
-                aliceMcMurtry, bobMcMurtry, carterMcMurtry, dariusMcMurtry, eyreMcMurtry
-        };
-        Household mcMurtryHousehold = new Household("McMurtry", mcMurtrys);
-        for (Person person: mcMurtrys) {
-            person.setHousehold(mcMurtryHousehold);
-        }
-        System.out.println("Number violating: " + yanHousehold.numCloseOutsideHousehold(10));
+        String a = "a";
+        String b = "a";
+
     }
-    private static Point randomPoint(){
-        return new Point(rand.nextInt(20), rand.nextInt(20));
-    }
+//    private static Point randomPoint(){
+//        return new Point(rand.nextInt(20), rand.nextInt(20));
+//    }
 
 }
